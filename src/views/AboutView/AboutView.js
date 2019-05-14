ca3d69f33dbc6c6b5d4abd753651fd3b23630c4d
Håvard Aasmo <havi95@gmail.com>
2019-05-14 14:57:56 +0200

    Fix indentation

--- src/views/AboutView/AboutView.js
+++ src/views/AboutView/AboutView.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const AboutView = props => {
-    return (
-        <div className="about">
-            <h1>What is Setback?</h1>
-            <p>Setback is a fast-paced, match-based, non-violent, sci-fi, multiplayer footracing FPS.</p>
-        </div>
-    );
-}
+  return (
+    <div className="about">
+      <h1>What is Setback?</h1>
+      <p>Setback is a fast-paced, match-based, non-violent, sci-fi, multiplayer footracing FPS.</p>
+    </div>
+  );
+};
 
 export default AboutView;
