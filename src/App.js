e3207b4b14991da37d4f74556142bd4c229f22b7
Håvard Aasmo <havi95@gmail.com>
2019-05-14 13:34:16 +0200

    Set up routing & create a simple 'about' view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import HomeView from './views/HomeView/HomeView';
+import AboutView from './views/AboutView/AboutView';
 
 function App() {
   return (
     <div className="app">
-      <Navbar />
-      <HomeView />
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={HomeView} />
+          <Route exact path="/about" component={AboutView} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
