1238ebddf9a7d008f5188a74d7951091735d1848
Håvard Aasmo <havi95@gmail.com>
2019-05-23 17:48:53 +0200

    Add analytics link click events to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import analytics from '../../analytics';
 
 const Footer = props => {
+
+  const onClick = (label) => {
+    analytics.event('Footer', 'Clicked Footer Link', label);
+  }
+
   return (
     <footer className="footer">
       <div className="footer__entry footer__copyright">
         &copy; {(new Date()).getFullYear()} Riddlebit AS
       </div>
-      <a className="footer__entry" href="https://www.riddlebit.net/contact">
+      <a
+        className="footer__entry"
+        href="https://www.riddlebit.net/contact"
+        onClick={() => onClick('Contact')}
+      >
         Contact Us
       </a>
-      <a className="footer__entry" href="https://www.riddlebit.net/privacy">
+      <a
+        className="footer__entry"
+        href="https://www.riddlebit.net/privacy"
+        onClick={() => onClick('Privacy')}
+      >
         Privacy Policy
       </a>
-      <a className="footer__entry" href="https://github.com/Riddlebit">
+      <a
+        className="footer__entry"
+        href="https://github.com/Riddlebit"
+        onClick={() => onClick('GitHub')}
+      >
         GitHub &nbsp;
         <i className="fab fa-github"></i>
       </a>
