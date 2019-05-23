0b483ae361ceebefa95328d73201be70c05357d9
Håvard Aasmo <havi95@gmail.com>
2019-05-23 14:57:25 +0200

    Fix test

--- src/analytics.js
+++ src/analytics.js
@@ -16,7 +16,7 @@ const initialize = () => {
   if (doNotTrack() || gaInitialized) {
     return;
   }
-  ReactGA.initialize(process.env.REACT_APP_GA_ID);
+  ReactGA.initialize(process.env.REACT_APP_GA_ID, {testMode: process.env.NODE_ENV === 'test'});
   ReactGA.set({anonymizeIp: true});
   gaInitialized = true;
 }
@@ -29,7 +29,7 @@ const pageview = (pathname) => {
 }
 
 const event = (category, action, label) => {
-  if (doNotTrack || !gaInitialized) {
+  if (doNotTrack() || !gaInitialized) {
     return;
   }
   ReactGA.event({
