16b7a74139be0e9d73080b7995d186deb50f3e1f
Håvard Aasmo <havi95@gmail.com>
2020-06-23 10:27:51 +0200

    Redirect blog to new riddleblog

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import HomeView from './views/HomeView/HomeView';
 import AboutView from './views/AboutView/AboutView';
-import BlogListView from './views/BlogListView/BlogListView';
-import BlogPostView from './views/BlogPostView/BlogPostView';
 import AnalyticsRouter from './components/AnalyticsRouter/AnalyticsRouter';
 import NotFoundView from './views/NotFoundView/NotFoundView';
 
@@ -16,8 +14,14 @@ function App() {
         <Switch>
           <Route exact path="/" component={HomeView} />
           <Route exact path="/about" component={AboutView} />
-          <Route exact path="/blog" component={BlogListView} />
-          <Route exact path="/blog/:postName" component={BlogPostView} />
+          <Route exact path="/blog" component={() => {
+            window.location.href = "https://blog.riddlebit.net";
+            return null;
+          }} />
+          <Route exact path="/blog/:postName" component={() => {
+            window.location.href = "https://blog.riddlebit.net";
+            return null;
+          }} />
           <Route component={NotFoundView} />
         </Switch>
         <AnalyticsRouter />
