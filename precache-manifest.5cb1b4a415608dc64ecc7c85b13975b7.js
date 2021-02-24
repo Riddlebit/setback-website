7cce9921f9d325b6f05fbe8b47b5b0fef72603b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-24 13:51:23 +0000

    Deploy setback.riddlebit.net to github.com/Riddlebit/setback-website.git:gh-pages

---
+++ precache-manifest.5cb1b4a415608dc64ecc7c85b13975b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7778d40265729f7581b9734113604ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b4804e4940d36645cf4",
+    "url": "/static/css/main.d5c44bb1.chunk.css"
+  },
+  {
+    "revision": "39dc9ce91493a66e9371",
+    "url": "/static/js/2.85e7d547.chunk.js"
+  },
+  {
+    "revision": "2b4804e4940d36645cf4",
+    "url": "/static/js/main.3f60f32e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ae525b481ccbf39922298c10621c63ad",
+    "url": "/static/media/riddlebit_logo.ae525b48.svg"
+  },
+  {
+    "revision": "8a656739ea2742d46dd053ef20668d73",
+    "url": "/static/media/setback_logo.8a656739.png"
+  }
+]);
